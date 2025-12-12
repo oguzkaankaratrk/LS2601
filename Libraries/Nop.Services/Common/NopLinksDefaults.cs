78ca01aef9070628f735d21409bf50a23b3ceee7
oguzkaankaratrk <oguzkaankaratrk@gmail.com>
2025-12-12 10:07:18 +0800

    Add project files.

---
+++ Libraries/Nop.Services/Common/NopLinksDefaults.cs
@@ -0,0 +1,450 @@
+﻿namespace Nop.Services.Common;
+
+/// <summary>
+/// Represents default links values
+/// </summary>
+public static partial class NopLinksDefaults
+{
+    /// <summary>
+    /// Represents default values of the official site URLs
+    /// </summary>
+    public static partial class OfficialSite
+    {
+        /// <summary>
+        /// Gets the main site page
+        /// </summary>
+        public static string Main => "#";
+
+        /// <summary>
+        /// Gets the boards page
+        /// </summary>
+        public static string Boards => "#";
+
+        /// <summary>
+        /// Gets the translations page
+        /// </summary>
+        public static string Translations => "#";
+
+        /// <summary>
+        /// Gets the copyright removal key page
+        /// </summary>
+        public static string CopyrightRemovalKey => "#";
+
+        /// <summary>
+        /// Gets the premium support services page
+        /// </summary>
+        public static string PremiumSupportServices => "#";
+
+        /// <summary>
+        /// Gets the marketplace page
+        /// </summary>
+        public static string Marketplace => "#";
+
+        /// <summary>
+        /// Gets the payment plugins page
+        /// </summary>
+        public static string PaymentPlugins => "#";
+
+        /// <summary>
+        /// Gets the shipping plugins page
+        /// </summary>
+        public static string ShippingPlugins => "#";
+
+        /// <summary>
+        /// Gets the tax plugins page
+        /// </summary>
+        public static string TaxPlugins => "#";
+
+        /// <summary>
+        /// Gets the themes page
+        /// </summary>
+        public static string Themes => "#";
+
+        /// <summary>
+        /// Gets the states and provinces package page
+        /// </summary>
+        public static string StatesPackage => "#";
+    }
+
+    /// <summary>
+    /// Represents default values of the docs site URLs
+    /// </summary>
+    public static partial class Docs
+    {
+        /// <summary>
+        /// Gets a URL of the main docs page
+        /// </summary>
+        public static string Main => "#";
+
+        /// <summary>
+        /// Gets a URL of the Products docs page
+        /// </summary>
+        public static string Products => "#";
+
+        /// <summary>
+        /// Gets a URL of the Categories docs page
+        /// </summary>
+        public static string Categories => "#";
+
+        /// <summary>
+        /// Gets a URL of the manufacturers docs page
+        /// </summary>
+        public static string Manufacturers => "#";
+
+        /// <summary>
+        /// Gets a URL of the product reviews docs page
+        /// </summary>
+        public static string ProductReviews => "#";
+
+        /// <summary>
+        /// Gets a URL of the product tags docs page
+        /// </summary>
+        public static string ProductTags => "#";
+
+        /// <summary>
+        /// Gets a URL of the product attributes docs page
+        /// </summary>
+        public static string ProductAttributes => "#";
+
+        /// <summary>
+        /// Gets a URL of the specification attributes docs page
+        /// </summary>
+        public static string SpecificationAttributes => "#";
+
+        /// <summary>
+        /// Gets a URL of the checkout attributes docs page
+        /// </summary>
+        public static string CheckoutAttributes => "#";
+
+        /// <summary>
+        /// Gets a URL of the orders docs page
+        /// </summary>
+        public static string Orders => "#";
+
+        /// <summary>
+        /// Gets a URL of the shipping management docs page
+        /// </summary>
+        public static string ShippingManagement => "#";
+
+        /// <summary>
+        /// Gets a URL of the return requests docs page
+        /// </summary>
+        public static string ReturnRequests => "#";
+
+        /// <summary>
+        /// Gets a URL of the recurring payments docs page
+        /// </summary>
+        public static string RecurringPayments => "#";
+
+        /// <summary>
+        /// Gets a URL of the shopping carts and wishlists docs page
+        /// </summary>
+        public static string ShoppingCartsAndWishlists => "#";
+
+        /// <summary>
+        /// Gets a URL of the gift cards docs page
+        /// </summary>
+        public static string GiftCards => "#";
+
+        /// <summary>
+        /// Gets a URL of the customers docs page
+        /// </summary>
+        public static string Customers => "#";
+
+        /// <summary>
+        /// Gets a URL of the customer roles docs page
+        /// </summary>
+        public static string CustomerRoles => "#";
+
+        /// <summary>
+        /// Gets a URL of the online customers docs page
+        /// </summary>
+        public static string OnlineCustomers => "#";
+
+        /// <summary>
+        /// Gets a URL of the vendor management docs page
+        /// </summary>
+        public static string VendorManagement => "#";
+
+        /// <summary>
+        /// Gets a URL of the activity log docs page
+        /// </summary>
+        public static string ActivityLog => "#";
+
+        /// <summary>
+        /// Gets a URL of the GDPR docs page
+        /// </summary>
+        public static string Gdpr => "#";
+
+        /// <summary>
+        /// Gets a URL of the discounts docs page
+        /// </summary>
+        public static string Discounts => "#";
+
+        /// <summary>
+        /// Gets a URL of the affiliates docs page
+        /// </summary>
+        public static string Affiliates => "#";
+
+        /// <summary>
+        /// Gets a URL of the email campaigns docs page
+        /// </summary>
+        public static string EmailCampaigns => "#";
+
+        /// <summary>
+        /// Gets a URL of the topics and pages docs page
+        /// </summary>
+        public static string TopicsPages => "#";
+
+        /// <summary>
+        /// Gets a URL of the message templates docs page
+        /// </summary>
+        public static string MessageTemplates => "#";
+
+        /// <summary>
+        /// Gets a URL of the news docs page
+        /// </summary>
+        public static string News => "#";
+
+        /// <summary>
+        /// Gets a URL of the blog docs page
+        /// </summary>
+        public static string Blog => "#";
+
+        /// <summary>
+        /// Gets a URL of the polls docs page
+        /// </summary>
+        public static string Polls => "#";
+
+        /// <summary>
+        /// Gets a URL of the forums docs page
+        /// </summary>
+        public static string Forums => "#";
+
+        /// <summary>
+        /// Gets a URL of the email accounts docs page
+        /// </summary>
+        public static string EmailAccounts => "#";
+
+        /// <summary>
+        /// Gets a URL of the multistore docs page
+        /// </summary>
+        public static string MultiStore => "#";
+
+        /// <summary>
+        /// Gets a URL of the countries and states docs page
+        /// </summary>
+        public static string CountriesStates => "#";
+
+        /// <summary>
+        /// Gets a URL of the localization docs page
+        /// </summary>
+        public static string Localization => "#";
+
+        /// <summary>
+        /// Gets a URL of the currencies docs page
+        /// </summary>
+        public static string Currencies => "#";
+
+        /// <summary>
+        /// Gets a URL of the payment methods docs page
+        /// </summary>
+        public static string PaymentMethods => "#";
+
+        /// <summary>
+        /// Gets a URL of the payment method restrictions docs page
+        /// </summary>
+        public static string PaymentMethodRestrictions => "#";
+
+        /// <summary>
+        /// Gets a URL of the tax providers docs page
+        /// </summary>
+        public static string TaxProviders => "#";
+
+        /// <summary>
+        /// Gets a URL of the shipping providers docs page
+        /// </summary>
+        public static string ShippingProviders => "#";
+
+        /// <summary>
+        /// Gets a URL of the warehouses docs page
+        /// </summary>
+        public static string Warehouses => "#";
+
+        /// <summary>
+        /// Gets a URL of the pickup points docs page
+        /// </summary>
+        public static string PickupPoints => "#";
+
+        /// <summary>
+        /// Gets a URL of the dates and ranges docs page
+        /// </summary>
+        public static string DatesAndRanges => "#";
+
+        /// <summary>
+        /// Gets a URL of the measures docs page
+        /// </summary>
+        public static string Measures => "#";
+
+        /// <summary>
+        /// Gets a URL of the ACL docs page
+        /// </summary>
+        public static string Acl => "#";
+
+        /// <summary>
+        /// Gets a URL of the external authentication docs page
+        /// </summary>
+        public static string ExternalAuthentication => "#";
+
+        /// <summary>
+        /// Gets a URL of the plugins docs page
+        /// </summary>
+        public static string Plugins => "#";
+
+        /// <summary>
+        /// Gets a URL of the log docs page
+        /// </summary>
+        public static string Log => "#";
+
+        /// <summary>
+        /// Gets a URL of the maintenance docs page
+        /// </summary>
+        public static string Maintenance => "#";
+
+        /// <summary>
+        /// Gets a URL of the message queue docs page
+        /// </summary>
+        public static string MessageQueue => "#";
+
+        /// <summary>
+        /// Gets a URL of the schedule tasks docs page
+        /// </summary>
+        public static string ScheduleTasks => "#";
+
+        /// <summary>
+        /// Gets a URL of the SEO optimization docs page
+        /// </summary>
+        public static string SeoOptimization => "#";
+
+        /// <summary>
+        /// Gets a URL of the templates docs page
+        /// </summary>
+        public static string Templates => "#";
+
+        /// <summary>
+        /// Gets a URL of the reports docs page
+        /// </summary>
+        public static string Reports => "#";
+
+        /// <summary>
+        /// Gets a URL of the tax management docs page
+        /// </summary>
+        public static string TaxManagement => "#";
+
+        /// <summary>
+        /// Gets a URL of the app settings docs page
+        /// </summary>
+        public static string AppSettings => "#";
+
+        /// <summary>
+        /// Gets a URL of the themes docs page
+        /// </summary>
+        public static string Themes => "#";
+
+        /// <summary>
+        /// Gets a URL of the SSL install docs page
+        /// </summary>
+        public static string SslInstall => "#";
+
+        /// <summary>
+        /// Gets a URL of the PayPal docs page
+        /// </summary>
+        public static string PayPal => "#";
+
+        /// <summary>
+        /// Gets a URL of the Avalara docs page
+        /// </summary>
+        public static string Avalara => "#";
+    }
+
+    /// <summary>
+    /// Represents default values of the UTM parameters
+    /// </summary>
+    public static partial class Utm
+    {
+        /// <summary>
+        /// Gets parameters used on admin area
+        /// </summary>
+        public static string OnAdmin => "#";
+
+        /// <summary>
+        /// Gets parameters used on admin area tour
+        /// </summary>
+        public static string OnAdminTour => "#";
+
+        /// <summary>
+        /// Gets parameters used on admin area tour
+        /// </summary>
+        public static string OnAdminTourDocs => "#";
+
+        /// <summary>
+        /// Gets parameters used on admin area log
+        /// </summary>
+        public static string OnAdminLog => "#";
+
+        /// <summary>
+        /// Gets parameters used on admin area log list
+        /// </summary>
+        public static string OnAdminLogList => "#";
+
+        /// <summary>
+        /// Gets parameters used on admin area footer
+        /// </summary>
+        public static string OnAdminFooter => "#";
+
+        /// <summary>
+        /// Gets parameters used on admin area countries page
+        /// </summary>
+        public static string OnAdminCountries => "#";
+
+        /// <summary>
+        /// Gets parameters used on admin area all plugins page
+        /// </summary>
+        public static string OnAdminAllPlugins => "#";
+
+        /// <summary>
+        /// Gets parameters used on admin area plugins feed page
+        /// </summary>
+        public static string OnAdminPluginsFeed => "#";
+
+        /// <summary>
+        /// Gets parameters used on admin area 'choose a theme' page
+        /// </summary>
+        public static string OnAdminThemes => "#";
+
+        /// <summary>
+        /// Gets parameters used on admin area payment plugins page
+        /// </summary>
+        public static string OnAdminPaymentPlugins => "#";
+
+        /// <summary>
+        /// Gets parameters used on admin area shipping plugins page
+        /// </summary>
+        public static string OnAdminShippingPlugins => "#";
+
+        /// <summary>
+        /// Gets parameters used on admin area tax plugins page
+        /// </summary>
+        public static string OnAdminTaxPlugins => "#";
+
+        /// <summary>
+        /// Gets parameters used on install
+        /// </summary>
+        public static string OnInstall => "#";
+
+        /// <summary>
+        /// Gets parameters used on admin area configuration steps block
+        /// </summary>
+        public static string OnAdminConfigurationSteps => "#";
+    }
+}
---
+++ Presentation/Nop.Web/Plugins/SevenSpikes.Theme.Voyage/Theme Upgrade/7Spikes_readme.txt
@@ -0,0 +1,3 @@
+﻿IMPORTANT. We don't use SQL upgrade scripts anymore. Upgrade is performed automatically with migrations (during the first application start).
+
+But in nopCommerce 4.60 the nopCommerce team created several upgrade scripts that you might consider running so please read more about this in the Readme.txt in the nopCommerce_UpgradeScripts folder.
